3455ba0860cc099ffcdadbd01b8d71c0420f5e37
AhmedElatreby <ahmedsport@hotmail.com>
2023-04-21 15:57:38 +0100

    add submitData function

--- lib/src/widgets/new_transactions.dart
+++ lib/src/widgets/new_transactions.dart
@@ -7,38 +7,50 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-            ),
-            TextButton(
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
-              child: Text('Add Transaction'),
-              style: TextButton.styleFrom(
-                onSurface: Colors.purple,
-              ),
-            ),
-          ],
-        ),
-      ),
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
     );
   }
-}
+
+    @override
+    Widget build(BuildContext context) {
+      return Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
+                onSubmitted: (value) => submitData(),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (value) => submitData(),
+              ),
+              TextButton(
+                onPressed: submitData,
+                child: Text('Add Transaction'),
+                style: TextButton.styleFrom(
+                  onSurface: Colors.purple,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
